759630bd2c9dc640d5d0c8a55cb13cf8ba44d22c
Udit Gupta <alugupta@gmail.com>
2015-05-10 18:13:50 -0400

    Added more data for writeup plot.

--- sims/plots/plot.py
+++ sims/plots/plot.py
@@ -4,12 +4,12 @@ import matplotlib.pyplot as plt
 def main():
     filename = "dbiGraph"
 
-    N       = 5
+    N       = 8
 
-    loadac  = (0.271126760563 , 0.336305213613  , 0.341455044612  , 0.338863636364  , 0.5)
-    storeac = (0.00146484375 , 0.0593011811024 , 0.0939597315436 , 0.0737781954887 , 0.5)
-    loaddc  = (0.184419014085 , 0.228095582911  , 0.231018359643  , 0.229159090909  , 0.5)
-    storedc = (0.00146484375 , 0.0509350393701 , 0.0734060402685 , 0.062734962406  , 0.5)
+    loadac  = (0.271126760563 ,  0.341455044612  , 0.338863636364  , 0.390  , 0.336305213613  ,0.319  , 0.336, 0.5)
+    storeac = (0.00146484375  ,  0.0939597315436 , 0.0737781954887 , 0.0449 , 0.0593011811024 ,0.09   , 0.0642, 0.5)
+    loaddc  = (0.184419014085 ,  0.231018359643  , 0.229159090909  , 0.260  , 0.228095582911  ,0.208  , 0.229, 0.5)
+    storedc = (0.00146484375  ,  0.0734060402685 , 0.062734962406  , 0.037  , 0.0509350393701 ,0.0663 , 0.059, 0.5)
     print loadac[0]
 
     ind = np.arange(N) # the x locations for the groups
@@ -26,7 +26,7 @@ def main():
     ax.set_ylabel('DBI Ratios')
     ax.set_title('DBI Ratios by MI-Bench Application')
     ax.set_xticks(ind+2*width)
-    ax.set_xticklabels(('basicmath', 'bitcount', 'jpeg', 'djikstra', 'encrypted'))
+    ax.set_xticklabels(('basicmath', 'jpeg', 'djikstra', 'fft', 'bitcount','blowfish', 'strsearch', 'enc.'))
 
     ax.legend( (rects1[0], rects2[0], rects3[0], rects4[0]), ('Load AC', 'Store AC', 'Load DC', 'Store DC') )
 
